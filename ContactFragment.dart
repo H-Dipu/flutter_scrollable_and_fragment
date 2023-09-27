a3add7d67160e570c916a398b00f62e131a0f8b0
MD HASANUZZAMAN DIPU <57557705+H-Dipu@users.noreply.github.com>
2023-09-27 08:51:29 +0600

    Create ContactFragment.dart

---
+++ ContactFragment.dart
@@ -0,0 +1,16 @@
+//import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+class ContactFragment extends StatelessWidget {
+  const ContactFragment({super.key});
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child:  const Center(
+        child: Text("Contact Fragment"),
+      ) ,
+    );
+  }
+}
