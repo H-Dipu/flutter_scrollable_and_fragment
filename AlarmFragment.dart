a9c603244647d62b039ba21afe1dd3e38ea9dea9
MD HASANUZZAMAN DIPU <57557705+H-Dipu@users.noreply.github.com>
2023-09-27 08:50:37 +0600

    Create AlarmFragment.dart

---
+++ AlarmFragment.dart
@@ -0,0 +1,15 @@
+//import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+class AlarmFragment extends StatelessWidget {
+  const AlarmFragment({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: const Center(
+        child: Text("Alarm Fragment"),
+      ),
+    );
+  }
+}
