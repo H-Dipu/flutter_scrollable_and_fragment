242ae7c4419b183d1122869a02f36bb78b787289
MD HASANUZZAMAN DIPU <57557705+H-Dipu@users.noreply.github.com>
2023-09-27 08:54:19 +0600

    Create HomeFragment.dart

---
+++ HomeFragment.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+class HomeFragment extends StatelessWidget {
+  const HomeFragment({super.key});
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child:  const Center(
+        child: Text("Home Fragment"),
+      ) ,
+    );
+  }
+}
