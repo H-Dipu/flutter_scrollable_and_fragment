8142b3755055e1d118b5bfba637c1e16fd58fced
MD HASANUZZAMAN DIPU <57557705+H-Dipu@users.noreply.github.com>
2023-09-27 08:51:05 +0600

    Create BalanceFragment.dart

---
+++ BalanceFragment.dart
@@ -0,0 +1,15 @@
+//import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+class BalanceFragment extends StatelessWidget {
+  const BalanceFragment({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: const Center(
+        child: Text("Balance Fragment"),
+      ),
+    );
+  }
+}
