e931623572eb4d7e89559dd14068ae458d180294
MD HASANUZZAMAN DIPU <57557705+H-Dipu@users.noreply.github.com>
2023-09-27 08:54:51 +0600

    Create PersonFragment.dart

---
+++ PersonFragment.dart
@@ -0,0 +1,16 @@
+//import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+class PersonFragment extends StatelessWidget {
+  const PersonFragment({super.key});
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child:  const Center(
+        child: Text("Person Fragment"),
+      ) ,
+    );
+  }
+}
